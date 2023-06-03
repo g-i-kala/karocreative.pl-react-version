2912da2dda9ead598d21c04808908c738d4c133f
g-i-kala <5star@wp.pl>
2023-06-03 12:52:16 +0200

    code button and screen added

---
+++ src/assets/images/index.js
@@ -0,0 +1,11 @@
+import screen_lgth from "./screen_lgth.png";
+import screen_kalafonia from "./screen_kalafonia.png";
+import screen_caro from "./screen_caro.png";
+import screen_wtd  from "./screen_wtd.png";
+
+export {
+    screen_caro,
+    screen_kalafonia,
+    screen_lgth,
+    screen_wtd
+}
--- src/components/MyWork/index.js
+++ src/components/MyWork/index.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import AnimateLetters from '../AnimateLetters';
-import './index.scss'
+import './index.scss';
+import { screen_wtd, screen_kalafonia, screen_lgth, screen_caro} from "../../assets/images";
 
 const MyWork = () => {
 
@@ -28,52 +29,101 @@ const MyWork = () => {
                 <div className="list__work">
                     
                     <div className="li__work">
-                        <a href="https://wakethedead.eu" className="li__work__link" target="_blank">wakethedead.eu</a>
-                        <ul className="li__work__spec">
-                            <li>graphic design & logo design</li>
-                            <li>prototype in Figma</li>
-                            <li>responsive & accessible design</li>
-                            <li>implemented & build using HTML5, CSS3 and JavaScript</li>
-                            <li>SEO optimised & analitccs integrated </li>
-                            <li>secure</li>
-                        </ul>
+                        <div className='li_work_img'>
+                            <a href="https://wakethedead.eu" className="li__work__link" target="_blank">
+                                <img src={screen_wtd} className='screen_img'/>
+                            </a>
+                        </div>
+                        <div className='li_work_text'>
+                            <a href="https://wakethedead.eu" className="li__work__link" target="_blank">wakethedead.eu</a>
+                            <ul className="li__work__spec">
+                                <li>graphic design & logo design</li>
+                                <li>prototype in Figma</li>
+                                <li>responsive & accessible design</li>
+                                <li>implemented & build using HTML5, CSS3 and JavaScript</li>
+                                <li>SEO optimised & analitccs integrated </li>
+                                <li>secure</li>
+                            </ul>
+                            <div className='backbutton__wrapper'>
+                                <a href='https://github.com/g-i-kala/wakethedead.eu' className='flat-button'>Code
+                                </a>
+                            </div>
+                        </div>
                     </div> 
+                    
 
                     <div className="li__work">
-                        <a href="https://karocreative.pl" className="li__work__link" target="_blank">karocreative.pl</a>
-                        <ul className="li__work__spec">
-                            <li>graphic design & logo design</li>
-                            <li>prototype in Figma</li>
-                            <li>responsive & accessible design</li>
-                            <li>implemented & build using React, CSS3</li>
-                            <li>secure</li>
-                        </ul>
-                    </div>
-                    
+                        <div className='li_work_img'>
+                            <a href="https://karocreative.pl" className="li__work__link" target="_blank">    
+                                <img src={screen_caro} className='screen_img'/>
+                            </a>    
+                        </div>
+                        <div className='li_work_text'> 
+                            <a href="https://karocreative.pl" className="li__work__link" target="_blank">karocreative.pl</a>
+                            <ul className="li__work__spec">
+                                <li>graphic design & logo design</li>
+                                <li>prototype in Figma</li>
+                                <li>responsive & accessible design</li>
+                                <li>implemented & build using React, CSS3</li>
+                                <li>secure</li>
+                            </ul>
+                            <div className='backbutton__wrapper'>
+                                <a href='https://github.com/g-i-kala/karocreative.pl-react-version' className='flat-button'>Code
+                                </a>
+                            </div>
+                        </div>
+                    </div>     
+
+
+
                     <div className="li__work">
-                        <a href="https://letgotohaveit.com" className="li__work__link" target="_blank">letgotohaveit.com</a>
-                       <ul className="li__work__spec">
-                            <li>graphic design & logo design</li>
-                            <li>prototype in Figma</li>
-                            <li>responsive & accessible design</li>
-                            <li>implemented in Wordpress</li>
-                            <li>build in Oxygen Builder </li>
-                            <li>SEO optimised & analitccs integrated </li>
-                            <li>secure</li>
-                        </ul>
+                        <div className='li_work_img'>
+                            <a href="https://letgotohaveit.com" className="li__work__link" target="_blank">
+                                <img src={screen_lgth} className='screen_img'/>
+                            </a>
+                        </div>
+                        <div className='li_work_text'>
+                            <a href="https://letgotohaveit.com" className="li__work__link" target="_blank">letgotohaveit.com</a>
+                                                   <ul className="li__work__spec">
+                                <li>graphic design & logo design</li>
+                                <li>prototype in Figma</li>
+                                <li>responsive & accessible design</li>
+                                <li>implemented in Wordpress</li>
+                                <li>build in Oxygen Builder </li>
+                                <li>SEO optimised & analitccs integrated </li>
+                                <li>secure</li>
+                            </ul>
+                            {/* <div className='backbutton__wrapper'>
+                                <a href='https://github.com/g-i-kala/karocreative.pl-react-version' className='flat-button'>Code
+                                </a>
+                            </div> */}
+                        </div>
                     </div>
+
                     <div className="li__work">
-                        <a href="https://kalafonia.com" className="li__work__link" target="_blank">kalafonia.com</a>
-                        <ul className="li__work__spec">
-                            <li>graphic design & logo design</li>
-                            <li>prototype in Figma</li>
-                            <li>responsive & accessible design</li>
-                            <li>implemented in Wordpress</li>
-                            <li>build in Oxygen Builder </li>
-                            <li>SEO optimised & analitccs integrated </li>
-                            <li>secure</li>
-                        </ul>
+                        <div className='li_work_img'>
+                            <a href="https://kalafonia.com" className="li__work__link" target="_blank"> 
+                                <img src={screen_kalafonia} className='screen_img'/>
+                            </a>
+                        </div>
+                        <div className='li_work_text'>
+                            <a href="https://kalafonia.com" className="li__work__link" target="_blank">kalafonia.com</a>
+                            <ul className="li__work__spec">
+                                <li>graphic design & logo design</li>
+                                <li>prototype in Figma</li>
+                                <li>responsive & accessible design</li>
+                                <li>implemented in Wordpress</li>
+                                <li>build in Oxygen Builder </li>
+                                <li>SEO optimised & analitccs integrated </li>
+                                <li>secure</li>
+                            </ul>
+                            {/* <div className='backbutton__wrapper'>
+                                <a href='https://github.com/g-i-kala/karocreative.pl-react-version' className='flat-button'>Code
+                                </a>
+                            </div> */}
+                        </div>
                     </div>
+
                             
                 </div>
             
